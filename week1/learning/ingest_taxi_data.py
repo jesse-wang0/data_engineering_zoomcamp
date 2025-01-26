36647158625362914cd06012359bbe9cb55575df
jesse <jesse21532@gmail.com>
2025-01-26 14:32:43 +1000

    Add basic functionality for reading in taxi csv into postgres DB via the CLI

---
+++ week1/learning/ingest_taxi_data.py
@@ -0,0 +1,37 @@
+import argparse
+import pandas as pd
+from sqlalchemy import create_engine
+
+def main(params):
+    engine = create_engine(f'postgresql://{params.username}:{params.password}@{params.host}:{params.port}/{params.db_name}')
+    engine.connect()
+
+    df_taxi = pd.read_csv("yellow_tripdata_2021-01.csv", nrows=100)
+    df_taxi['tpep_pickup_datetime'] = pd.to_datetime(df_taxi['tpep_pickup_datetime'])
+    df_taxi['tpep_dropoff_datetime'] = pd.to_datetime(df_taxi['tpep_dropoff_datetime'])
+
+    df_taxi.head(0).to_sql(name=params.table_name, con=engine, if_exists='replace')
+
+    for chunk in pd.read_csv("yellow_tripdata_2021-01.csv", chunksize=100000):
+        print("Start inserting chunk")
+        chunk['tpep_pickup_datetime'] = pd.to_datetime(chunk['tpep_pickup_datetime'])
+        chunk['tpep_dropoff_datetime'] = pd.to_datetime(chunk['tpep_dropoff_datetime'])
+        chunk.to_sql(name='yellow_taxi_data', con=engine, if_exists='append')
+        print("Finish inserting chunk")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+                    prog='TaxiDataIngest',
+                    description='Ingests data from a given link into chosen database'
+                    )
+
+    parser.add_argument('-U', '--username', help='Username for postgres')
+    parser.add_argument('-P', '--password', help='Password for postgres')
+    parser.add_argument('-H', '--host', help='Host for postgres')
+    parser.add_argument('-p', '--port', help='Port for postgres')
+    parser.add_argument('-d', '--db_name', help='Name of database in postgres')
+    parser.add_argument('-t', '--table_name', help='Name of table in postgres')
+    parser.add_argument('-u', '--url', help='URL of CSV file to download')
+    args = parser.parse_args()
+
+    main(args)
